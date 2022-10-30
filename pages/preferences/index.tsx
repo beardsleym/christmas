e3766c412cbafd30548b81ebb891948b1ab5c5ca
beardsleym <matthew@beardsley.com.au>
2022-10-30 16:15:17 -0500

    preferences page save daysArray

--- pages/preferences/index.tsx
+++ pages/preferences/index.tsx
@@ -5,12 +5,25 @@ import { Main } from "../../components/Main";
 import { PreferencesCard } from "../../components/PreferencesCard";
 import { Header } from "../../components/Header";
 import { useLocalStorage } from "@mantine/hooks";
+import { useEffect } from "react";
 
 export default function Home() {
   const [value, setValue] = useLocalStorage<string[]>({
     key: "categories",
-    defaultValue: [],
+    defaultValue: undefined,
   });
+  const [array, setArray] = useLocalStorage<Number[]>({
+    key: "daysArray",
+    defaultValue: undefined,
+  });
+
+  useEffect(() => {
+    const arr: Number[] = Array.from({ length: 31 }, (_, i) => i + 1).sort(
+      () => Math.random() - 0.5
+    );
+    setArray(arr);
+  }, [setArray]);
+
   return (
     <>
       <Header />
