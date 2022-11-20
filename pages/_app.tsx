81857d1f99623c841f9e5e019b2e9cb1338e3336
beardsleym <matthew@beardsley.com.au>
2022-11-20 18:23:00 -0500

    feat(analytics): add cloudflare analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { ChakraProvider } from "@chakra-ui/react";
 import { NextIntlProvider } from "next-intl";
 import { theme } from "../styles/theme";
@@ -7,6 +8,10 @@ import { theme } from "../styles/theme";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Script
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token": "fcb8aa7e3e5043ccb9dd62b418d82ea0"}'
+      />
       <NextIntlProvider messages={pageProps.messages}>
         <Component {...pageProps} />
       </NextIntlProvider>
