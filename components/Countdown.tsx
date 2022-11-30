5ac7f3e9300392ebaddabadd69bcd36e3a36a7e8
beardsleym <matthew@beardsley.com.au>
2022-11-30 10:52:37 -0500

    french countdown

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -1,5 +1,6 @@
 import { Center, Text, Heading, Box } from "@chakra-ui/react";
 import { Player } from "@lottiefiles/react-lottie-player";
+import { useTranslations } from "next-intl";
 
 let today = new Date();
 let christmasYear = today.getFullYear();
@@ -15,32 +16,37 @@ let remainingDays = Math.ceil(
   (christmasDate.getTime() - today.getTime()) / dayMilliseconds
 );
 
-export const Countdown: React.FC = () => (
-  <Center h="100vh" flexDirection="column">
-    <Box position="relative" textAlign="center">
-      <Player
-        src="https://assets3.lottiefiles.com/packages/lf20_i7y3y8fi.json"
-        className="player"
-        autoplay
-        loop
-        style={{ height: "256px", width: "256px" }}
-      />
-      <Heading
-        mt={4}
-        color="white"
-        fontSize="2xl"
-        position="absolute"
-        top="34%"
-        right="46%"
-      >
-        {remainingDays}
-      </Heading>
-    </Box>
-    <Text mt={4} color="white" fontSize="2xl">
-      Day{remainingDays > 1 && "s"} till December
-    </Text>
-    <Text mt={4} color="white" fontSize="md">
-      We&apos;ll be waiting for you here.
-    </Text>
-  </Center>
-);
+export const Countdown = () => {
+  const t = useTranslations("Countdown");
+
+  return (
+    <Center h="100vh" flexDirection="column">
+      <Box position="relative" textAlign="center">
+        <Player
+          src="https://assets3.lottiefiles.com/packages/lf20_i7y3y8fi.json"
+          className="player"
+          autoplay
+          loop
+          style={{ height: "256px", width: "256px" }}
+        />
+        <Heading
+          mt={4}
+          color="white"
+          fontSize="2xl"
+          position="absolute"
+          top="34%"
+          right="46%"
+        >
+          {remainingDays}
+        </Heading>
+      </Box>
+      <Text mt={4} color="white" fontSize="2xl">
+        {t("day")}
+        {remainingDays > 1 && "s"} {t("december")}
+      </Text>
+      <Text mt={4} color="white" fontSize="md">
+        {t("body")}
+      </Text>
+    </Center>
+  );
+};
